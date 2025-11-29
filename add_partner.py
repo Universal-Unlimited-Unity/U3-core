42c63bbe6b1b8dd8809db6b52be703017e448002
UUU, BluePain <adamaakif23@gmail.com>
2025-11-29 16:12:22 +0100

    Ensure unique UnityId for partners
    Added functionality to ensure UnityId is unique by checking existing entries and updating if necessary.

--- add_partner.py
+++ add_partner.py
@@ -8,7 +8,7 @@ def init_db():
   cursor = conn.cursor()
 
   cursor.execute(""" CREATE TABLE IF NOT EXISTS partners (
-    First_Name TEXT,
+    First_Name TEXT UNIQUE,
     Middle_Name TEXT,
     Last_Name TEXT,
     Age INTEGER,
@@ -19,10 +19,33 @@ def init_db():
   conn.commit()
   conn.close()
 
+def UnityId_Used(p: U3PartnerModel) -> bool:
+  init_db()
+  conn = sqlite3.connect(db_path)
+  cursor = conn.cursor()
+  cursor.execute(""" SELECT * FROM partners
+                    WHERE UnityId = ? """, (p.UnityId,))
+  result = cursor.fetchone()
+  conn.close()
+  return result is not None
+  
+def Update_UnityId(p: U3PartnerModel):
+  Edit = 0
+  UnityId_OG = p.UnityId
+  while UnityId_Used(p):
+    Edit = Edit + sum(list(map(ord, p.FirstName+p.LastName))) // 85
+    NewUnityId = UnityId_OG + str(Edit + sum(list(map(ord, p.FirstName+p.LastName))) // 85)
+    p.UnityId = NewUnityId
+  
 def Add_Partner(p: U3PartnerModel):
   init_db()
   conn = sqlite3.connect(db_path)
   cursor = conn.cursor()
+
+  # check if unityId is unique, if not generate a new one.
+
+  if (UnityId_Used(p)):
+    Update_UnityId(p)
 
   cursor.execute(""" INSERT INTO partners(First_Name,
     Middle_Name,
