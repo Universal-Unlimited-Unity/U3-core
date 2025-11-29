98c434d0ce7e3fe4589b51acfc38ad50004b06b4
UUU, BluePain <adamaakif23@gmail.com>
2025-11-29 17:00:58 +0100

    Fix UnityId uniqueness and refactor related functions

--- add_partner.py
+++ add_partner.py
@@ -8,31 +8,30 @@ def init_db():
   cursor = conn.cursor()
 
   cursor.execute(""" CREATE TABLE IF NOT EXISTS partners (
-    First_Name TEXT UNIQUE,
+    First_Name TEXT,
     Middle_Name TEXT,
     Last_Name TEXT,
     Age INTEGER,
     Gender TEXT,
     Loyalty TEXT,
-    UnityId TEXT) """)
+    UnityId TEXT UNIQUE) """)
 
   conn.commit()
   conn.close()
 
-def UnityId_Used(p: U3PartnerModel) -> bool:
-  init_db()
+def UnityId_Used(UnityId: str) -> bool:
   conn = sqlite3.connect(db_path)
   cursor = conn.cursor()
   cursor.execute(""" SELECT * FROM partners
-                    WHERE UnityId = ? """, (p.UnityId,))
+                    WHERE UnityId = ? """, (UnityId,))
   result = cursor.fetchone()
   conn.close()
   return result is not None
   
 def Update_UnityId(p: U3PartnerModel):
-  Edit = 0
+  Edit = 1
   UnityId_OG = p.UnityId
-  while UnityId_Used(p):
+  while UnityId_Used(p.UnityId):
     Edit = Edit + sum(list(map(ord, p.FirstName+p.LastName))) // 85
     NewUnityId = UnityId_OG + str(Edit + sum(list(map(ord, p.FirstName+p.LastName))) // 85)
     p.UnityId = NewUnityId
@@ -44,7 +43,7 @@ def Add_Partner(p: U3PartnerModel):
 
   # check if unityId is unique, if not generate a new one.
 
-  if (UnityId_Used(p)):
+  if (UnityId_Used(p.UnityId)):
     Update_UnityId(p)
 
   cursor.execute(""" INSERT INTO partners(First_Name,
@@ -64,9 +63,3 @@ def Add_Partner(p: U3PartnerModel):
                   p.UnityId))
   conn.commit()
   conn.close()
-  
-  
-
-  
-    
-    
